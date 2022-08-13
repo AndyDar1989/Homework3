8e4b22498686a7bb0878366695066812854c94de
AndrewDar <geologalone@gmail.com>
2022-08-13 11:42:18 +0300

    create task19

---
+++ task19/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+string Palindrome(int num)
+{
+    if (num/10000==num%10 && (num-(num/10000)*10000)/1000==(num%100-num%10)/10) return "Yes";
+    return "No";
+}
+
+Console.WriteLine("Enter five-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string result = Palindrome(n);
+Console.WriteLine(result);
+
+
